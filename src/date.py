cdaa210d3faebabbc54903604eb90d8909c5543d
Alex Dawn <al_4242@hotmail.co.uk>
2020-09-24 17:06:55 +0100

    Fix off by one error in imperial date

--- src/date.py
+++ src/date.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from math import floor
+from math import floor, ceil
 
 
 YEAR_HOURS = 365 * 24
@@ -11,13 +11,19 @@ def get_imperial_date(datetime):
     timedelta = date - year_start
     hours = 24 * timedelta.days + datetime.hour + datetime.minute / 60
     year_frac = floor(1000 * hours / YEAR_HOURS)
-    millenium = floor(date.year / 1000)
-    return "{} {:03d} {:03d}M{}".format(
+    millenium = ceil(date.year / 1000)
+    return "{} {:03d} {:03d}.M{}".format(
         0, year_frac, year_part, millenium)
 
+
+def get_imperial_date_from_date(year, month, day):
+    return get_imperial_date(datetime(year, month, day))
 
 def get_imperial_date_now():
     return get_imperial_date(datetime.now())
 
 if __name__ == "__main__":
-    print(get_imperial_date_now())
+    print("now", get_imperial_date_now())
+    for year in range(1900, 2021):
+        for month in range(1, 13):
+            print("{}-{}".format(year, month), get_imperial_date_from_date(year, month, 1))
